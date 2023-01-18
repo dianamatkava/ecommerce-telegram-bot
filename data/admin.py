1d7efaeef28b5f53750f2e8b9f95dbb77015e806
Diana Matkava <88348384+dianamatkava@users.noreply.github.com>
2023-01-18 12:31:48 +0100

    Create all models in ADmin

--- data/admin.py
+++ data/admin.py
@@ -23,4 +23,111 @@ class SubCategoryAdmin(admin.ModelAdmin):
     list_display_links = ['px_id', 'name']
     
     
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    fields = ['name', 'description']
+    list_display = ['px_id', 'name', 'description']
+    list_filter = ['name']
+    search_fields = ['name', 'ru_name']
+    list_display_links = ['px_id', 'name']
     
+    
+@admin.register(Offer)
+class OfferAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {
+            'fields': ('subcategory','category')
+        }),
+        ('Prices and Amount', {
+            'fields': (
+                'sell_cur', 'buy_cur',
+                'margin', 'price_per_cur', 
+            ),
+        }),
+        ('User Data', {
+            'fields': (
+                'username', 'score', 'user_timezone', 
+                'payment_method_label', 'description',
+                'warranty', 'offer_detail'
+            ),
+        }),
+    )
+    
+    list_display = [
+        'px_id', 'subcategory', 'sell_cur', 'buy_cur', 'margin',
+        'username', 'score', 'user_timezone',  'category', 
+    ]
+    list_filter = ['category', 'sell_cur', 'buy_cur', 'username']
+    search_fields = ['subcategory', 'category', 'username']
+    list_display_links = ['px_id', 'subcategory']    
+    
+
+
+class OfferInline(admin.StackedInline):
+    model = Offer
+    fieldsets = (
+        (None, {
+            'fields': ('subcategory','category')
+        }),
+        ('Prices and Amount', {
+            'fields': (
+                'sell_cur', 'buy_cur',
+                'margin', 'price_per_cur', 
+            ),
+        }),
+        ('User Data', {
+            'fields': (
+                'username', 'score', 'user_timezone', 
+                'payment_method_label', 'description',
+                'warranty', 'offer_detail'
+            ),
+        }),
+    )
+
+
+@admin.register(OfferDetail)
+class OfferDetailAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ('Amount', {
+            'fields': (
+                'predefined_amount',
+                'fiat_amount_range_min',
+                'fiat_amount_range_max'
+            )
+        }),
+        ('User Feedback', {
+            'fields': (
+                'feedback_positive', 
+                'feedback_negative'
+            ),
+        }),
+    )
+    inlines = (OfferInline, )
+
+
+@admin.register(GiftOrder)
+class GiftOrderAdmin(admin.ModelAdmin):
+    list_display = [
+        'id', 'status', 'offer', 'user',
+        'discount', 'amount', 'created_at'
+    ]
+    list_filter = ['status']
+    search_fields = ['user', 'offer', 'id']
+    list_display_links = ['id', 'offer']
+
+
+@admin.register(TgUser)
+class TgUserAdmin(admin.ModelAdmin):
+    list_display = [
+        'tg_id', 'username', 'language_code',
+        'currency', 'is_admin', 'is_bot'
+    ]
+    search_fields = ['username', 'tg_id']
+    list_display_links = ['tg_id', 'username']
+
+
+@admin.register(CurrencyDetail)
+class CurrencyDetailAdmin(admin.ModelAdmin):
+    list_display = ['code', 'country']
+    search_fields = ['code', 'country']
+    list_display_links = ['code', 'country']
