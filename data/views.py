eccc7bf8d45a45f774a97077091b520a2a1b4cdc
Diana Matkava <88348384+dianamatkava@users.noreply.github.com>
2023-01-21 18:56:33 +0100

    PayPal IPN

--- data/views.py
+++ data/views.py
@@ -1,17 +1,30 @@
 
 import os
+import json
 from dotenv import load_dotenv
 from telegram.ext.updater import Updater
 
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import TgUser, CurrencyDetail
-
+from django.views.decorators.csrf import csrf_exempt
 load_dotenv()
 
-def home(request):
-    # user = TgUser.objects.get(id=1)
-    # updater = Updater(os.getenv('TELEGRAM_TOKEN', None))
-    # updater.bot.sendMessage(chat_id=user.tg_id, text='Hello there!')
 
-    return HttpResponse('')
+# @csrf_exempt
+# def home1(request):
+#     print(request.payment_status)
+
+#     return HttpResponse(200)
+
+from paypal.standard.models import ST_PP_COMPLETED
+from paypal.standard.ipn.signals import valid_ipn_received
+from django.dispatch import receiver
+
+@csrf_exempt
+@receiver(valid_ipn_received)
+def home(sender, **kwargs):
+    print(kwargs)
+    ipn_obj = sender
+    if ipn_obj.payment_status == "Completed":
+           print(ipn_obj)
