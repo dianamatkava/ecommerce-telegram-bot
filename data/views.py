f673c9b1966c1410d528c6918ee9ba2b360bd299
Diana Matkava <88348384+dianamatkava@users.noreply.github.com>
2023-02-01 15:40:37 +0000

    update IPN

--- data/views.py
+++ data/views.py
@@ -1,4 +1,6 @@
+import sys
 import os
+import logging
 import requests
 from dotenv import load_dotenv
 from django.http import HttpResponse
@@ -6,8 +8,16 @@ from django.views.decorators.csrf import csrf_exempt
 from .models import Payment, PaymentAddress, GiftOrder
 from .utils import get_fiat_amount
 
-
+sys.stdout = open('/root/gunicorn.log', 'a')
 load_dotenv()
+
+logging.basicConfig(filename='/root/gunicorn.log',
+                    filemode='a',
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=logging.DEBUG)
+
+logger = logging.getLogger(__name__)
 
 
 @csrf_exempt
@@ -16,8 +26,9 @@ def home(request):
         URL = os.getenv('PAYPAL_IPN')
         params = {'cmd': '_notify-validate'}
         params.update(request.POST.dict())
+        logger.info(request.POST.get('mc_gross'))
         response = requests.post(URL, params=params)
-
+        logger.info(f"IPN TEST {response.text}, {request.POST.get('mc_gross')}")
         if response.text == 'VERIFIED':
             amount = request.POST.get('mc_gross')
             status = request.POST.get('payment_status', False)
@@ -25,8 +36,9 @@ def home(request):
             currency = request.POST.get('mc_currency')
             receiver_email = request.POST.get('receiver_email')
             payment_date = request.POST.get('payment_date')
-            print(request.POST.__dict__)
-            
+            logger.info(request.POST.__dict__)
+            logger.info(f'\n\n {status}')
+
             if status == 'Completed':
                 user = None
                 fiat_amount = None
@@ -41,10 +53,10 @@ def home(request):
                         fiat_amount = get_fiat_amount(
                             order.offer.buy_cur, currency, amount
                         )
-                
+
                 address = PaymentAddress.objects.filter(
                     address=receiver_email
-                ) 
+                )
                 if not address:
                     address = None
 
